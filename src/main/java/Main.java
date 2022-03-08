8a35ac343e55d5f7c37b2731447b20b4169aa165
SamLaz11 <Ivan1234DadkaYr20@gmail.com>
2022-03-08 23:05:45 +0200

    Finish remove logic middle chare

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         /*Удалить в каждом слове букву стоящею по середине, если нет середины-то ниче не делать.*/
-        new SentenceToWordSplitter();
+        final SentenceToWordSplitter splitter = new SentenceToWordSplitter();
+        final String[] split = splitter.split("Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).");
+        final MiddleCharRemover middleCharRemover = new MiddleCharRemover();
+        final String[] result = middleCharRemover.remove(split);
+
+        System.out.println(Arrays.toString(result));
     }
 }
